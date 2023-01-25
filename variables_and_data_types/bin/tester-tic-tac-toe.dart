a2a18574febd76a47d74be32600599b60032654f
dmknapp2385 <dmknapp2385@gmail.com>
2023-01-25 08:46:21 -0700

    testing functions for tic tac toe game

--- variables_and_data_types/bin/tester-tic-tac-toe.dart
+++ variables_and_data_types/bin/tester-tic-tac-toe.dart
@@ -1,36 +1,57 @@
 // program to test if there is a winner for a tic tac toe board given as a list of three lists
 
 void main () {
-    final tttList = [[0,1,2], 
-                    [0, 1, 0], 
-                    [0, 1, 1]];
+    final tttList = [[2, 2,1], 
+                    [1, 1, 1], 
+                    [0, 2, 1]];
 
-    // varibale to hold winner
-    var winner;
+    gameWinner(tttList);
+}
 
-    // first check if there are horizontal winners
-    for (var list in tttList) {
-        // if list has 0, no winners
-        if (list.contains(0)){
-            winner = 'none';
-        } else if (list[0] == list[1] && list[1] == list[2]) {
-            winner = list[0];
-            break;
-        } else {
-            winner = 'none';
+// function to get game winner
+// accepts a list of lists and first checks rows, then columns, then diagonals to see if there is a winner
+void gameWinner(List<List<int>> board) {
+    // check rows for winners
+    if (rowCheck(board)){
+        print('Row Wins!');
+    } else if (rowCheck(transpose(board))) {
+        print('Column Wins!');
+    } else if (rowCheck(diagonals(board))){
+        print('Diagonal Wins!');
+    } else {
+        print('Draw.');
+    }
+}
+
+
+// function to check for a row winner
+// accepts a list of lists and returns true if there is a winner
+
+bool rowCheck(List<List<int>> board) {
+    for (List<int> row in board) {
+        if (row.toSet().length == 1) {
+            return true;
         }
     }
-    
-    //chech for horizontal winners
-    //first for loop to look at all the first indicies of all three lists
-    for (var i = 0; i< 3; i++) {
-        if (tttList[0][i] == tttList[1][i] && tttList[1][i] == tttList[2][i] && tttList[2][i] != 0) {
-            winner = tttList[0][i];
-            break;
-        } else {
-            winner = 'none';
-        }
-    }
+    return false;
+}
 
-    print(winner);
+
+
+// function to transpose board in order to check for column winners
+
+List<List<int>> transpose (List<List<int>> board) {
+    return [
+        for (var i = 0; i <board.length; i++) [for (var row in board) row[i]]
+    ];
+}
+
+
+// function to return a list of the diagonals
+
+List<List<int>> diagonals(List<List<int>> board) {
+    return [
+        for (var i = 0; i < board.length; i ++) [board[i][i]],
+        for (var i = 0; i < board.length; i++) [board[i].reversed.toList()[i]]
+    ];
 }
